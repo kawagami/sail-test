dac5240f96af5820c6863d21c82f94cd62a961f3
kawagami <joelai1988@gmail.com>
2022-03-06 19:28:52 +0800

    add google account login done

--- README.md
+++ README.md
@@ -2,12 +2,5 @@
 * with redis & mysql
 
 ## record
-* in project directory
-* start project
-    * ./vendor/bin/sail up
-* end project
-    * ./vendor/bin/sail up
-* artisan command
-    * ./vendor/bin/sail php artisan ACTION
-* composer command
-    * ./vendor/bin/sail composer ACTION
+
+### Socialite
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Google login
+Route::get('/login/google', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle'])->name('login.google');
+Route::get('/login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
+
+// facebook login
+Route::get('/login/facebook', [App\Http\Controllers\Auth\LoginController::class, 'redirectToFacebook'])->name('login.facebook');
+Route::get('/login/facebook/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleFacebookCallback']);
+
+// Github login
+Route::get('/login/github', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGithub'])->name('login.github');
+Route::get('/login/github/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGithubCallback']);
